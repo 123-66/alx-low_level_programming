8052593f55cbdc8dab8b66f9f22e24443f4198b0
123-66 <nanaamafremah113@gmail.com>
2023-02-17 04:09:09 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,18 +14,3 @@ printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
 
-7
-
-#!/bin/bash                                          
-gcc -S -masm=intel $CFILE -o "${CFILE%.*}.s"
-
-8
-#include <unistd.h>
-/**                                                   
-* main - prints a string and standard error
- * Return: 1                                          
-*/
-int main(void)                                       {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-}
